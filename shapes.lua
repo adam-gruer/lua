1fd3a1deadaaf02240d07e708efe6ce74ef2d3c0
Adam Gruer <gruera@rch.org.au>
2018-06-14 16:03:11 +1000

    added some funcitons

--- shapes.lua
+++ shapes.lua
@@ -16,3 +16,17 @@ function complement (r)
         return not r(x, y)
     end
 end
+
+function union (r1, r2)
+    return function(x, y)
+        return r1(x, y) or r2(x, y)
+    end
+end
+
+function intersection (r1, r2)
+    return function (x, y)
+        return r1(x, y) and r2(x, y)
+    end
+end
+
+function
