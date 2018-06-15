ed648a3accc5fa46e2645fdde43b8cfce3cda583
Adam Gruer <gruera@rch.org.au>
2018-06-15 10:41:49 +1000

    completed changes

--- shapes.lua
+++ shapes.lua
@@ -29,4 +29,29 @@ function intersection (r1, r2)
     end
 end
 
-function
+function difference (r1, r2)
+    return function (x, y)
+        return r1(x,y) and not r2(x, y)
+    end
+end
+
+function translate (r, dx, dy)
+    return function(x, y)
+        return r(x - dx, y - dy)
+    end
+end
+
+
+function plot (r, M, N)
+    io.write("P1\n", M, " ", N, "\n")  --header
+    for i = 1, N do                  -- for each line
+        local y = (N - i*2)/N
+        for j = 1, M do                -- for each column
+            local x = (j*2 - M)/M
+            io.write(r(x,y) and "1" or "0")
+        end
+        io.write("\n")
+    end
+end
+
+
